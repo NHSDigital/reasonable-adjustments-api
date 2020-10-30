fe787a68ddcdda4f77ff11c9f3d6af5e99540222
Adrian Clare <adrian.clare@airelogic.com>
2020-10-30 14:59:10 +0000

    small changes

--- api_tests/config_files/environments.py
+++ api_tests/config_files/environments.py
@@ -1,51 +1,55 @@
 import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 # Configure Test Environment
-def get_env(variable_name: str, default: str = "") -> str:
-    """Returns a environment variable"""
-    try:
-        return os.environ[variable_name]
-    except KeyError:
-        return default
+# def get_env(variable_name: str, default: str = "") -> str:
+#     """Returns a environment variable"""
+#     try:
+#         return os.environ[variable_name]
+#     except KeyError:
+#         return default
 
 
 ENV = {
     'apps': {
         'internal_testing_internal_dev': {
-            'client_id': get_env('CLIENT_ID'),
-            'client_secret': get_env('CLIENT_SECRET'),
-            'redirect_url': get_env('REDIRECT_URI')
+            'client_id': os.environ.get('CLIENT_ID'),
+            'client_secret': os.environ.get('CLIENT_SECRET'),
+            'redirect_url': os.environ.get('REDIRECT_URI')
         },
         'missing_asid': {
-            'client_id': get_env('MISSING_ASID_CLIENT_ID'),
-            'client_secret': get_env('MISSING_ASID_CLIENT_SECRET'),
+            'client_id': os.environ.get('MISSING_ASID_CLIENT_ID'),
+            'client_secret': os.environ.get('MISSING_ASID_CLIENT_SECRET'),
             'redirect_url': 'https://example.com/callback'
         },
         'missing_ods': {
-            'client_id': get_env('MISSING_ODS_CLIENT_ID'),
-            'client_secret': get_env('MISSING_ODS_CLIENT_SECRET'),
+            'client_id': os.environ.get('MISSING_ODS_CLIENT_ID'),
+            'client_secret': os.environ.get('MISSING_ODS_CLIENT_SECRET'),
             'redirect_url': 'https://example.com/callback'
         }
     },
     'oauth': {
-        'apigee_client_id': get_env('APIGEE_CLIENT_ID'),
-        'base_url': get_env('BASE_URL'),
-        'client_id': get_env('CLIENT_ID'),
-        'client_secret': get_env('CLIENT_SECRET'),
-        'redirect_uri': get_env('REDIRECT_URI'),
-        'authenticate_url': get_env('AUTHENTICATE_URL'),
+        'apigee_client_id': os.environ.get('APIGEE_CLIENT_ID'),
+        'base_url': os.environ.get('BASE_URL'),
+        'client_id': os.environ.get('CLIENT_ID'),
+        'client_secret': os.environ.get('CLIENT_SECRET'),
+        'redirect_uri': os.environ.get('REDIRECT_URI'),
+        'authenticate_url': os.environ.get('AUTHENTICATE_URL'),
     },
     'apigee': {
-        'organisation': get_env('APIGEE_ORGANISATION'),
-        'base_url': get_env('APIGEE_API_URL'),
-        'api_authentication': get_env('APIGEE_API_AUTHENTICATION'),
-        'username': get_env('APIGEE_USERNAME'),
-        'password': get_env('APIGEE_PASSWORD'),
+        'organisation': os.environ.get('APIGEE_ORGANISATION'),
+        'base_url': os.environ.get('APIGEE_API_URL'),
+        'api_authentication': os.environ.get('APIGEE_API_AUTHENTICATION'),
+        'username': os.environ.get('APIGEE_USERNAME'),
+        'password': os.environ.get('APIGEE_PASSWORD'),
     },
     'reasonable_adjustments': {
-        'base_url': get_env('REASONABLE_ADJUSTMENTS_BASE_URL'),
-        'proxy_path': get_env('REASONABLE_ADJUSTMENTS_PROXY_PATH'),
-        'proxy_name': get_env('REASONABLE_ADJUSTMENTS_PROXY_NAME'),
+        'base_url': os.environ.get('REASONABLE_ADJUSTMENTS_BASE_URL'),
+        'proxy_path': os.environ.get('REASONABLE_ADJUSTMENTS_PROXY_PATH'),
+        'proxy_name': os.environ.get('REASONABLE_ADJUSTMENTS_PROXY_NAME'),
     }
 }
--- api_tests/tests/reasonable_adjustments/test_jwt.py
+++ api_tests/tests/reasonable_adjustments/test_jwt.py
@@ -3,7 +3,7 @@ import jwt
 import requests
 
 from assertpy import assert_that
-from api_tests.config_files.config import REASONABLE_ADJUSTMENTS_PROXY, REASONABLE_ADJUSTMENTS_CONSENT
+from api_tests.config_files.config import REASONABLE_ADJUSTMENTS_PROXY_NAME, REASONABLE_ADJUSTMENTS_CONSENT
 from api_tests.scripts.apigee_api import ApigeeDebugApi
 
 @pytest.mark.usefixtures("setup")
@@ -15,7 +15,7 @@ class TestJwtSuite:
     @pytest.mark.usefixtures('get_token')
     def test_jwt(self):
         # Given
-        debug_session = ApigeeDebugApi(REASONABLE_ADJUSTMENTS_PROXY)
+        debug_session = ApigeeDebugApi(REASONABLE_ADJUSTMENTS_PROXY_NAME)
         expected_jwt_claims = {
             'reason_for_request': 'directcare',
             'scope': 'patient=test&category=test&status=test',
@@ -24,7 +24,7 @@ class TestJwtSuite:
             'requesting_user': 'https://fhir.nhs.uk/Id/sds-role-profile-id|test',
             'sub': 'https://fhir.nhs.uk/Id/sds-role-profile-id|test',
             'iss': 'http://api.service.nhs.uk',
-            'aud': f'/{REASONABLE_ADJUSTMENTS_PROXY}/consent'
+            'aud': f'/{REASONABLE_ADJUSTMENTS_PROXY_NAME}/consent'
         }
         
         # When
