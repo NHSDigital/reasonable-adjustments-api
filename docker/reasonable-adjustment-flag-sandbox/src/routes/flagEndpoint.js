44ff4138599d8897e6afa4ff12c8ab9484c3b617
RyanThomas1214 <ryan_j_thomas@hotmail.co.uk>
2021-09-21 14:12:50 +0100

    AMB-838 use new test patient in tests/spec

--- docker/reasonable-adjustment-flag-sandbox/src/routes/flagEndpoint.js
+++ docker/reasonable-adjustment-flag-sandbox/src/routes/flagEndpoint.js
@@ -2,7 +2,7 @@ const flagGet =  {
     method: 'GET',
     path: '/Flag',
     handler: (request, h) => {
-        if (request.query["patient"] != '9692247317') {
+        if (request.query["patient"] != '9693892283') {
             const path = 'flagGETerror.json'
             return h.response(h.file(path)).code(404);
         }
