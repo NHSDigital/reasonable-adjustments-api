db079d43605152bf7aba169e3b0324b4569ae38d
Jack McCaffrey <jack.mccaffrey1@nhs.net>
2023-01-09 18:15:16 +0000

    AMB-1368: Changed from `apim_test_utils` to newer `pytest-nhsd-apim`

--- api_tests/tests/reasonable_adjustment_flag/test_auth.py
+++ api_tests/tests/reasonable_adjustment_flag/test_auth.py
@@ -14,27 +14,28 @@ class TestAuthCasesSuite:
 
     @pytest.mark.integration
     @pytest.mark.smoke
-    @pytest.mark.asyncio
-    def test_asid_auth(self, get_token_client_credentials):
-        # Given
-        token = get_token_client_credentials["access_token"]
+    @pytest.mark.nhsd_apim_authorization(
+        {
+            "access": "healthcare_worker",
+            "level": "aal3",
+            # "login_form": {"username": "aal3"},
+        }
+    )
+    def test_asid_auth(self, nhsd_apim_proxy_url, nhsd_apim_auth_headers):
         expected_status_code = 200
 
         # When
         response = requests.get(
-            url=config.REASONABLE_ADJUSTMENTS_CONSENT,
+            url=f"{nhsd_apim_proxy_url}/Consent",
             params={
                 'patient': self.existing_patient,
                 'category': 'https://fhir.nhs.uk/STU3/CodeSystem/RARecord-FlagCategory-1|NRAF',
                 'status': 'active'
             },
-            headers={
-                'Authorization': f'Bearer {token}',
-                'NHSD-Session-URID': config.TEST_NHSD_SESSION_URID,
-                'x-request-id': str(uuid.uuid4()),
-                'accept': 'application/json'
-            }
+            headers=nhsd_apim_auth_headers
         )
+
+        import pdb; pdb.set_trace()
 
         # Then
         assert_that(expected_status_code).is_equal_to(response.status_code)
