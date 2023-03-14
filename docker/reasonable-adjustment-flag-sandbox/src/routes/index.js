7667582f5dd73c8964a0220e6fdd37f11c0b9a30
almas-liaqat <Almas.liaqat@accenture.com>
2023-03-14 17:46:32 +0000

    Adding UnderlyingConditionList path to APIs

--- docker/reasonable-adjustment-flag-sandbox/src/routes/index.js
+++ docker/reasonable-adjustment-flag-sandbox/src/routes/index.js
@@ -1,9 +1,10 @@
 const consentEndpoint = require('./consentEndpoint');
 const flagEndpont = require('./flagEndpoint');
+const underlyingConditionListEndpoint=require('./underlyingConditionListEndpoint');
 const listEndpoint = require('./listEndpoint');
 const removerarecordEndpoint = require('./removerarecordEndpoint');
 const statusEndpoint = require('./statusEndpoint');
 
-const routes = [].concat(consentEndpoint, flagEndpont, listEndpoint, removerarecordEndpoint, statusEndpoint)
+const routes = [].concat(consentEndpoint, flagEndpont, listEndpoint, removerarecordEndpoint, statusEndpoint, underlyingConditionListEndpoint)
 
 module.exports = routes
